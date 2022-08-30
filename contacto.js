b113d67a6d3625c24918753a0cc10d8dce210ab4
Fernando <ferweb.reyna@gmail.com>
2022-08-30 00:21:49 -0300

    transicion

--- contacto.js
+++ contacto.js
@@ -1,6 +1,6 @@
 const validarCampos = ()=> {
-    let nombre = document.getElementById("nombre").value;
-    let nombreError = document.getElementById("nombreError");
+    let from_name = document.getElementById("from_name").value;
+    let from_nameError = document.getElementById("from_nameError");
     let telefono = document.getElementById("telefono").value;
     let telefonoError = document.getElementById("telefonoError");
     let email = document.getElementById("email").value;
@@ -11,13 +11,13 @@ const validarCampos = ()=> {
     let comentarioError = document.getElementById("comentarioError");
 
     //trim para sacarle los espacios del medio 
-    if (nombre.trim() == ""){
-        nombreError.innerHTML ="Falta completar el campo Nombre!";
-        nombreError.className="text-danger";
+    if (from_name.trim() == ""){
+        from_nameError.innerHTML ="Falta completar el campo Nombre!";
+        from_nameError.className="text-danger";
         return false;
         //se coloca un return false por las dudas que tengamos el campo vacio "nombre", asi lo detiene ahi 
     }else{
-        nombreError.innerHTML ="";
+        from_nameError.innerHTML ="";
         //se coloca para el caso que este completo correctamente no me aparezca el error
     }
     
@@ -54,17 +54,17 @@ const validarCampos = ()=> {
 
 
     //llamamos a funcion guardar datos
-    guadarDatos(nombre, telefono, email, password,comentario);
+    guadarDatos(from_name, telefono, email, password,comentario);
 
     //llamamos a funcion recuperar datos / informacion de todos los datos que guardamos en la localStorage
     const datosUsuario=recuperarDatos();
-    document.getElementById("resultado").innerHTML=`<p>Nombre:${datosUsuario.nombreUsuario}<br> Telefono: ${datosUsuario.telefonoUsuario}<br> Email: ${datosUsuario.emailUsuario}<br>Password: ${datosUsuario.passwordUsuario}<br>Comentario: ${datosUsuario.comentarioUsuario}</p>`;
+    document.getElementById("resultado").innerHTML=`<p>Nombre:${datosUsuario.from_nameUsuario}<br> Telefono: ${datosUsuario.telefonoUsuario}<br> Email: ${datosUsuario.emailUsuario}<br>Password: ${datosUsuario.passwordUsuario}<br>Comentario: ${datosUsuario.comentarioUsuario}</p>`;
     document.getElementById("resultado").className="bg-light fs-4";
 
 }
 //objeto Guardamos los datos
-const guadarDatos =(nombre, telefono, email, password)=> {
-    const datos = {nombreUsuario:nombre, telefonoUsuario: telefono, emailUsuario: email, passwordUsuario:password, comentarioUsuario:comentario}
+const guadarDatos =(from_name, telefono, email, password,comentario)=> {
+    const datos = {from_nameUsuario:from_name, telefonoUsuario: telefono, emailUsuario: email, passwordUsuario:password, comentarioUsuario:comentario}
     localStorage.setItem("datosUsuario", JSON.stringify(datos));
 }
 // objeto recuperamos los datos, proceso inverso al anterior
@@ -73,5 +73,28 @@ const recuperarDatos =()=>{
 }
 
 
-document.getElementById("btnIngresar").onclick = validarCampos;
 document.getElementById("btnEnviar").onclick = validarCampos;
+
+
+
+
+
+const btnEmailjs = document.getElementById('buttonEmailjs');
+
+document.getElementById('form').addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btnEmailjs.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_8amglno';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+        btnEmailjs.value = 'Enviar Email';
+      alert('Enviado!');
+    }, (err) => {
+      btnEmailjs.value = 'Enviar Email';
+      alert(JSON.stringify(err));
+    });
+});
